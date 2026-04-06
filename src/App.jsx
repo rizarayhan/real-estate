8d69592bf8b9739dae15616a35a550a8a4512fd6
rzryhn <rizarayhanw@gmail.com>
2026-04-06 16:53:43 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+      <About />
+      <Projects />
+      <Testimonials />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
